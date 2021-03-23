c699e510f23591593f827f9a09508d73feea93f8
Simo-Pekka Koskinen <spkoskinen@Simo-Pekkas-MacBook-Pro.local>
2021-03-23 10:46:15 +0200

    Delete boatload of commented out code

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,45 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 import { ApolloCache, gql, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
-import { format } from "date-fns";
 
 import { AddWorkoutMutation } from "apollo/queries";
 import WorkoutForm from "components/WorkoutForm";
-import Exercise from "pages/create/Exercise";
-import { Workout, WorkoutExercise, WorkoutSet } from "types/types";
-import { PrimaryButton, Button } from "components/Button";
-import Link from "components/Link";
-
-const Container = styled.div`
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-
-  > * {
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-  }
-`;
-
-const Title = styled.header`
-  font-size: 1.5rem;
-`;
-
-const Exercises = styled.div`
-  > * {
-    box-sizing: border-box;
-    margin-top: 0.25rem;
-    margin-bottom: 0.25rem;
-  }
-`;
-
-const Actions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2rem;
-`;
+import { Workout, WorkoutExercise } from "types/types";
 
 const updateWorkoutCache = (cache: ApolloCache<Workout>, result) => {
   cache.modify({
@@ -84,55 +49,6 @@ export default function CreateWorkout() {
   const saveWorkout = React.useCallback(() => {
     addWorkout({ variables: { workout } });
   }, [workout]);
-  /**const activateSet = React.useCallback(() => {
-    exercises.push({
-      type: "",
-      sets: [
-        {
-          order: 1,
-          reps: 0,
-          weight: 0,
-        },
-      ],
-    });
-    setExercises([...exercises]);
-  }, [exercises, setExercises]);
-  const addSet = React.useCallback(
-    (exercise: WorkoutExercise) => {
-      const updatedExercises = exercises.map((item) => {
-        if (item.type !== exercise)
-        const maxOrder = Math.max(
-          ...exercise.sets.map((set: WorkoutSet) => set.order)
-        );
-        item.type === exercise.type
-          ? { ...item, sets: item.sets.concat({ reps: 0, weight: 0 }) }
-          : item
-      }
-        
-      );
-      setExercises([...updatedExercises]);
-    },
-    [exercises, setExercises]
-  );
-  return (
-    <Container>
-      <Title>Workout {format(new Date(), "dd.MM.yyyy")}</Title>
-      <Actions>
-        <Link href="/" label="Back" />
-        <PrimaryButton onClick={saveWorkout} label="Save" />
-      </Actions>
-      <Button onClick={activateSet} label="New exercise" />
-      <Exercises>
-        {exercises.map((exercise: WorkoutExercise, index: number) => (
-          <Exercise
-            exercise={exercise}
-            onAddSetPressed={addSet}
-            order={index + 1}
-          />
-        ))}
-      </Exercises>
-    </Container>
-  );**/
 
   const addExercise = React.useCallback(() => {
     const maxOrder = Math.max(
